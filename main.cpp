4a7f973cab349de289854dace920d88ddf8dabf2
blagovest.zhivkov <blagovest.zhivkov@playboxtechnology.com>
2015-10-21 00:32:33 +0300

    dump env vars

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
+<<<<<<< HEAD
 void display_usage() {
     printf("DUMMY [-h]\n");
     printf("-h\t\tDisplay this help message.\n");
 }
 
-int main(int argc, char* argv[]) {
+void dump_pchar_array(char* envp[]) {
+    int index = 0;
+    while(envp[index] != 0) {
+        printf("envp[%d] = %s", index, envp[index]);
+        ++index;
+    }
+}
+
+int main(int argc, char* argv[], char* envp[]) {
     if(argc <= 1) {
         display_usage();
         return 0;
     }
+
     return 0;
 }
