d72b54b7d7e0de05b0ca141aa5816c0ef81c82f7
blagovest.zhivkov <blagovest.zhivkov@playboxtechnology.com>
2015-10-21 00:34:53 +0300

    display usage

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+void display_usage() {
+    printf("DUMMY [-h]\n");
+    printf("-h\t\tDisplay this help message.\n");
+}
+
 int main(int argc, char* argv[]) {
+    if(argc <= 1) {
+        display_usage();
+        return 0;
+    }
     return 0;
 }
